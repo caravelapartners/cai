3b7efacd54549af953e831ac58ec078e1841ee33
Miguel Ferreira <miguelf@caravelapartners.com>
2024-09-12 13:45:54 +0000

    bug fixes

--- neural.py
+++ neural.py
@@ -19,15 +19,16 @@ import neuralgcm
 gcs = gcsfs.GCSFileSystem() # use default credentials
 
 def load_checkpoint(model_path):
-    with gcs.open(model_path, 'rb') as f:
-        ckpt = pickle.load(f)
+    with open(model_path, 'rb') as fh:
+        ckpt = pickle.load(fh)
 
     model = neuralgcm.PressureLevelModel.from_checkpoint(ckpt)
     return model
 
-def load_initial_conditions(model, modeldate, base_path='gs://weather_website/zarr/'):
-    init_path = f"{base_path}{modeldate.strftime('%Y%m%d%H')}/"
-    init_ds = xarray.open_zarr(gcs.get_mapper(init_path), chunks=None)
+def load_initial_conditions(model, modeldate, base_path):
+    init_path = f"{base_path}{modeldate.strftime('%Y%m%d%H')}"
+
+    init_ds = xarray.open_zarr(init_path, chunks=None)
     init_ds = init_ds.sortby('level')
 
     ic_grid = spherical_harmonic.Grid(
@@ -51,13 +52,13 @@ def run_model(modeldate, model, base_path, output_path):
     timedelta = np.timedelta64(1, 'h') * inner_steps
     times = (np.arange(outer_steps) * inner_steps)  # time axis in hours
 
-    ic_ds = load_initial_conditions(model, modeldate, base_path+'/analysis/zarr/')
+    ic_ds = load_initial_conditions(model, modeldate, base_path+'analysis/zarr/')
 
     # initialize model state
     inputs = model.inputs_from_xarray(ic_ds.isel(time=0))
     input_forcings = model.forcings_from_xarray(ic_ds.isel(time=0))
     rng_key = jax.random.key(42)  # optional for deterministic models
-    print(rng_key)
+
     initial_state = model.encode(inputs, input_forcings, rng_key)
 
     # use persistence for forcing variables (SST and sea ice cover)
@@ -68,15 +69,14 @@ def run_model(modeldate, model, base_path, output_path):
                                             steps=outer_steps, timedelta=timedelta, start_with_input=True)
     predictions_ds = model.data_to_xarray(predictions, times=times)
 
+    predictions_ds.to_netcdf(os.path.expanduser(f"{output_path}{modeldate.strftime('%Y%m%d%H')}.nc"))
+
     end_time = time.time()  # Record the end time
     execution_time = end_time - start_time  # Calculate the execution time
     print(f"Ran {modeldate.strftime('%Y-%m-%d %HZ')} \t Execution time: {execution_time:.3f} seconds")
 
-    with gcs.open(f"{output_path}{modeldate.strftime('%Y%m%d%H')}.nc", 'wb') as f:
-        predictions_ds.to_netcdf(f)
 
-
-def run(modeldates, base_path= '/mnt/disks/era5land/', output_path = 'gs://compostus-stor/weather/cai/fcast/'):
+def run(modeldates, base_path= '/mnt/disks/era5land/', output_path = '~/output/'):
     model = load_checkpoint(base_path + '/model/ens1pt4.pkl')
     for modeldate in modeldates:
         print(f'Running model for {modeldate}')
@@ -89,7 +89,7 @@ def main():
     parser.add_argument("--enddate", help="enddate", action="store", default='2024-08-30')
     args = parser.parse_args()
 
-    model_dts = pd.date_range(args.startdate, args.enddate, freq='12H')
+    model_dts = pd.date_range(args.startdate, args.enddate, freq='12h')
     run(model_dts)
 
 if __name__ == "__main__":
